2f99bb25978fdae3b6789eff291fef76b7fe601c
Aliq1413 <alliarakan@yahoo.com>
2022-01-06 07:27:53 -0600

    add Activity

--- app/src/main/java/com/example/taskstimerapp/CategoryActivity.kt
+++ app/src/main/java/com/example/taskstimerapp/CategoryActivity.kt
@@ -1,21 +1,23 @@
 package com.example.taskstimerapp
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-class MainActivity : AppCompatActivity() {
+
+class CategoryActivity : AppCompatActivity() {
     private val navigasjonen = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.ic_house -> {
+            R.id.ic_section -> {
                 return@OnNavigationItemSelectedListener false
             }
-            R.id.ic_task -> {
-                val intent1 = Intent(this@MainActivity, TasksActivity::class.java)
+            R.id.ic_house -> {
+                val intent1 = Intent(this@CategoryActivity, MainActivity::class.java)
                 startActivity(intent1)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.ic_section -> {
-                val intent2 = Intent(this@MainActivity, CategoryActivity::class.java)
+            R.id.ic_task -> {
+                val intent2 = Intent(this@CategoryActivity, TasksActivity::class.java)
                 startActivity(intent2)
                 return@OnNavigationItemSelectedListener true
             }
@@ -27,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_category2)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigation.setOnNavigationItemSelectedListener(navigasjonen)
--- app/src/main/java/com/example/taskstimerapp/TasksActivity.kt
+++ app/src/main/java/com/example/taskstimerapp/TasksActivity.kt
@@ -1,22 +1,24 @@
 package com.example.taskstimerapp
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-class MainActivity : AppCompatActivity() {
+
+class TasksActivity : AppCompatActivity() {
     private val navigasjonen = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.ic_house -> {
+            R.id.ic_task -> {
                 return@OnNavigationItemSelectedListener false
             }
-            R.id.ic_task -> {
-                val intent1 = Intent(this@MainActivity, TasksActivity::class.java)
-                startActivity(intent1)
+            R.id.ic_house-> {
+                val intent = Intent(this@TasksActivity, MainActivity::class.java)
+                startActivity(intent)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.ic_section -> {
-                val intent2 = Intent(this@MainActivity, CategoryActivity::class.java)
-                startActivity(intent2)
+                val intent = Intent(this@TasksActivity, CategoryActivity::class.java)
+                startActivity(intent)
                 return@OnNavigationItemSelectedListener true
             }
 
@@ -27,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tasks)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNavigation.setOnNavigationItemSelectedListener(navigasjonen)
